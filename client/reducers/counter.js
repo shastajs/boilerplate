cc3d782e0e0954f99484009a9c2d492d30ff539b
Steve Lacy <me@slacy.me>
2016-02-23 16:41:18 -0800

    add reset

--- client/reducers/counter.js
+++ client/reducers/counter.js
@@ -6,3 +6,4 @@ export const increment = (state, { payload = 1 }) =>
   state.update('count', v => v + payload)
 export const decrement = (state, { payload = 1 }) =>
   state.update('count', v => v - payload)
+export const reset = () => initialState
