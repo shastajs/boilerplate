1eb33c17d2aeeb4936dbcf7e8cd3d97f2cc6ea3a
Contra <contra@maricopa.edu>
2016-03-14 14:40:31 -0700

    clean up resource gen

--- client/core/actions.js
+++ client/core/actions.js
@@ -1,8 +1,7 @@
 import { actions as routeActions } from 'shasta-router'
 import { createActions, createReducerActions } from 'shasta'
 import createAPIActions from 'redux-sutro'
-import initialState from 'core/store/initialState'
-import { dispatch } from 'core/store'
+import { dispatch, getState } from 'core/store'
 import localActions from 'actions/.lookup'
 import localReducers from 'reducers/.lookup'
 
@@ -10,5 +9,5 @@ export default createActions({
   ...localActions,
   ...routeActions,
   ...createReducerActions(localReducers),
-  api: createAPIActions(initialState.get('resources'))
+  api: createAPIActions(getState().get('resources'))
 }, dispatch)
